581453875c4acc6fdebac39667b45d4e0850c685
Rafael Ramos <junior07.rrn@hotmail.com>
2024-01-29 12:44:26 -0500

    changed port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App</h1>`);
